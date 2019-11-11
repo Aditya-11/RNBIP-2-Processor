410987e4607e18ac1947dacd9b058eac9b77955a
sid-137 <sid-137@users.noreply.github.com>
2019-11-12 01:05:18 +0530

    updated data memory

--- mem_data.v
+++ mem_data.v
@@ -3,20 +3,24 @@
 module DataMemory(
     input   [7:0]   SP_in,
     input   [7:0]   R0_in,
-    input   [7:0]   dataIn,
+    input   [7:0]   NPC_in,
+    input   [7:0]   RN_in,
     output  [7:0]   dataOut,
     input           RD,
     input           WR,
     input           S2,
+    input           S5,
     input           clk
 );
 
     reg [7:0] dataMem[255:0];
     wire [7:0] address;
-    
-    assign address = S2 ? SP_in : R0_in;
+    wire [7:0] dataIn;
+
+    assign address = S2 ? SP_in : R0_in;    //MUX2
+    assign dataIn = S5 ? RN_in : NPC_in;    //MUX5
     assign dataOut = dataMem[address];
-    
+
     always @(posedge clk)
     begin
         if (WR)
@@ -24,7 +28,7 @@ module DataMemory(
     end
 
     initial begin
-        
+
     end
-    
+
 endmodule // DataMemory
